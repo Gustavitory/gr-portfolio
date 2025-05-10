9ce6364b96944746c0f6c2c288735c684f104e20
Gustavo Riera <ggrieraya@gmail.com>
2025-05-09 23:40:25 -0400

    First commit

---
+++ components/layout/components/navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full z-50 bg-black/90">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center py-5 mx-auto c-space">
+          <a
+            href="#"
+            className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
+          >
+            Gustavo
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{tsx,jsx,js,ts}",
+    "./pages/**/*.{tsx,jsx,js,ts}",
+    "./components/**/*.{tsx,jsx,js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        test: "#ed6aff",
+      },
+    },
+  },
+  plugins: [],
+} as Config;
